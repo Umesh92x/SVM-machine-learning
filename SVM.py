0de4087c5e71f4415cd2a1a50ed1fc02955a595d
Umesh <37169232+dinesh92x@users.noreply.github.com>
2018-03-24 14:47:09 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Link path the csv to your dirctory folder
-dataset=pd.read_csv('C://Users//vinod//PycharmProjects//Excercise//Udemy//Social_Network_Ads.csv')
+dataset=pd.read_csv('Social_Network_Ads.csv')
 
 X=dataset.iloc[:,[2,3]].values
 y=dataset.iloc[:,4].values
